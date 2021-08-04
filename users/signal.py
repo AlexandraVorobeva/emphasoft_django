fb12fc933d4e8c4ba38b40ce668a526a931e93c0
Alexandra Vorobeva <65659002+SparklingAcidity@users.noreply.github.com>
2021-08-04 16:56:48 +0300

    Delete signal.py

--- users/signal.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-
-
-@receiver(post_save, sender=User)
-def create_user(sender, instance, created, **kwargs):
-    if created:
-        User.objects.create(user=instance)
